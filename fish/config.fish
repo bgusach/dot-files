bdc7903547cc462b935881927a2b3ea78085615b
bgusach <bgusach@gmail.com>
2017-09-18 22:58:38 +0200

    make fzf use rg per default

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,10 @@
 # Symlink to ~/.config/fish/config.fish
 
 alias ll "ls -lh --color=auto"
-alias tmux "tmux -2"
 alias vim "nvim"
 alias gpg "gpg2"
+
+set FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 
 # fish_vi_key_bindings
 # bind -M insert \cf accept-autosuggestion
