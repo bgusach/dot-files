ec0d14f4c3864b10ebc3f5e70196d4bc11d65668
bgusach <bgusach@gmail.com>
2017-10-01 21:53:38 +0200

    pyenv stuff

--- fish/config.fish
+++ fish/config.fish
@@ -6,5 +6,10 @@ alias gpg "gpg2"
 
 set FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
 
+
+set -x PATH "/home/troll/.pyenv/bin" $PATH
+status --is-interactive; and . (pyenv init -|psub)
+status --is-interactive; and . (pyenv virtualenv-init -|psub)
+
 # fish_vi_key_bindings
 # bind -M insert \cf accept-autosuggestion
