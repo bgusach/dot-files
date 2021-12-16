fe5bfca24c9f10802f5178f1483f3742db1843c1
Bor Gonzalez-Usach <bgusach@gmail.com>
2021-12-16 14:17:59 +0100

    Improve provisioning script

--- basic-bootstrap.sh
+++ basic-bootstrap.sh
@@ -3,16 +3,19 @@ sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt install fish
 chsh --shell `which fish`
+
+ln --force $PWD/config.fish ~/.config/fish/conf.d/config.fish
 echo "Log out and in again"
 
 
 # Git
 sudo apt install --yes git
-ln --symbolic --force $PWD/git/gitconfig ~/.gitconfig
-ln --symbolic --force $PWD/git/gitignore-global ~/.gitignore-global
+ln --force $PWD/git/gitconfig ~/.gitconfig
+ln --force $PWD/git/gitignore-global ~/.gitignore-global
 
 # Other goodies
 sudo apt install --yes ripgrep
 sudo apt install --yes neovim
 sudo apt install --yes fd-find
+sudo apt install --yes fzf
 
--- fish/config.fish
+++ fish/config.fish
@@ -1,21 +1,14 @@
-# Symlink to ~/.config/fish/config.fish
+# Symlink to ~/.config/fish/conf.d/config.fish
 
 alias ll "ls -lh --color=auto"
 alias vim "nvim"
 alias gpg "gpg2"
 alias xstart "xdg-open"
+alias fd "fdfind"
 
-set FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
-
-
-set -x PATH "/$HOME/.pyenv/bin" $PATH
-status --is-interactive; and . (pyenv init -|psub)
-status --is-interactive; and . (pyenv virtualenv-init -|psub)
+set FZF_DEFAULT_COMMAND 'fd --type f'
 
 set -x PATH "/snap/bin" $PATH
 # fish_vi_key_bindings
 # bind -M insert \cf accept-autosuggestion
-
-set -x PATH "$HOME/miniconda3/bin" $PATH
-source (conda info --root)/etc/fish/conf.d/conda.fish
 
