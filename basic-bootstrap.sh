2d9d95c3bd4692d0c18595b54f00a03e07f1ad6f
Bor Gonzalez-Usach <bgusach@gmail.com>
2021-12-17 08:50:39 +0100

    Improvements

--- basic-bootstrap.sh
+++ basic-bootstrap.sh
@@ -1,3 +1,5 @@
+# $PWD must be parent of this file!
+
 # Fish
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
@@ -5,13 +7,19 @@ sudo apt install fish
 chsh --shell `which fish`
 
 ln --force $PWD/config.fish ~/.config/fish/conf.d/config.fish
+ln --symbolic $PWD/fish/functions ~/.config/fish
 echo "Log out and in again"
-
 
 # Git
 sudo apt install --yes git
 ln --force $PWD/git/gitconfig ~/.gitconfig
 ln --force $PWD/git/gitignore-global ~/.gitignore-global
+mkdir --parents ~/.local/bin
+
+# Use 'fd diff-highlight /usr' if this path changes...
+ln -s /usr/share/doc/git/contrib/diff-highlight/diff-highlight ~/.local/bin/diff-highlight
+sudo chmod +x ~/.local/bin/diff-highlight
+
 
 # Other goodies
 sudo apt install --yes ripgrep
--- fish/config.fish
+++ fish/config.fish
@@ -8,7 +8,7 @@ alias fd "fdfind"
 
 set FZF_DEFAULT_COMMAND 'fd --type f'
 
-set -x PATH "/snap/bin" $PATH
+set -x PATH "$HOME/.local/bin" $PATH
 # fish_vi_key_bindings
 # bind -M insert \cf accept-autosuggestion
 
