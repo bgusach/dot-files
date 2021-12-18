9d8447bcb6c0c73944e975d86af68f0fd06f3bbe
Bor Gonzalez-Usach <bgusach@gmail.com>
2021-12-18 14:52:04 +0100

    Prettify stuff

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -15,41 +15,35 @@ end
 
 
 function fish_prompt
-  set -l last_command_status $status
+  set -l last_status $status
   set -l cwd (prompt_pwd)
 
-  set -l ahead    "↑"
-  set -l behind   "↓"
-  set -l diverged "⥄ "
-  set -l dirty    "±"
-  set -l none     "◦"
+  set -l ahead "↑"
+  set -l behind "↓"
+  set -l diverged "⥄"
+  set -l dirty "±"
+  set -l none ""
 
-  set -l normal_color     (set_color normal)
-  set -l success_color    (set_color $fish_pager_color_progress 2> /dev/null; or set_color cyan)
-  set -l error_color      (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
-  set -l directory_color  (set_color $fish_color_quote 2> /dev/null; or set_color brown)
+  set -l normal_color (set_color normal)
+  set -l success_color (set_color $fish_pager_color_progress 2> /dev/null; or set_color cyan)
+  set -l directory_color (set_color $fish_color_quote 2> /dev/null; or set_color brown)
   set -l repository_color (set_color $fish_color_cwd 2> /dev/null; or set_color green)
+  set -l error_color (set_color $fish_color_error 2> /dev/null; or set_color red --bold)
 
-  echo -n -s $directory_color $cwd $normal_color
+  echo -ns $directory_color $cwd $normal_color
 
   if git_is_repo
-    echo -n -s " (" $repository_color (git_branch_name) $normal_color " "
+    echo -ns " (" $repository_color (git_branch_name) $normal_color
 
     if git_is_touched
-      echo -n -s $dirty
+      echo -ns $dirty
     else
-      echo -n -s (git_ahead $ahead $behind $diverged $none)
+      echo -ns (git_ahead $ahead $behind $diverged $none)
     end
 
-    echo -n -s ")"
+    echo -ns ")"
 
   end
 
-#  if test $last_command_status -eq 0
-#    echo -n -s $success_color $fish $normal_color
-#  else
-#    echo -n -s $error_color $fish $normal_color
-#  end
-
-  echo -n -s " "
+  echo -ns " "
 end
