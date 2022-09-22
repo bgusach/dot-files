34b356615f4c458361fcfd104dfb85db6f576a7a
Bor Gonzalez-Usach <bgusach@gmail.com>
2022-09-22 11:38:16 +0200

    Further improvements

--- basic-bootstrap.sh
+++ basic-bootstrap.sh
@@ -1,29 +1,31 @@
+#/bin/bash
 # $PWD must be parent of this file!
 
 # Fish
+echo ">>> Installing fish"
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
 sudo apt install fish
+echo ">>> Setting fish as default shell (requires interaction)"
 chsh --shell `which fish`
 
-ln --force $PWD/config.fish ~/.config/fish/conf.d/config.fish
-ln --symbolic $PWD/fish/functions ~/.config/fish
-echo "Log out and in again"
+ln --symbolic $PWD/fish/config.fish ~/.config/fish/conf.d/config.fish
+ln --symbolic $PWD/fish/functions ~/.config/fish/functions
 
+echo ">>> Installing goodies"
 # Git
-sudo apt install --yes git
-ln --force $PWD/git/gitconfig ~/.gitconfig
-ln --force $PWD/git/gitignore-global ~/.gitignore-global
-mkdir --parents ~/.local/bin
-
-# Use 'fd diff-highlight /usr' if this path changes...
-ln -s /usr/share/doc/git/contrib/diff-highlight/diff-highlight ~/.local/bin/diff-highlight
-sudo chmod +x ~/.local/bin/diff-highlight
-
+sudo apt install -y git
+ln -s $PWD/git/gitconfig ~/.gitconfig
+ln -s $PWD/git/gitignore-global ~/.gitignore-global
 
 # Other goodies
-sudo apt install -y ripgrep
-sudo apt install -y neovim
-sudo apt install -y fd-find
-sudo apt install -y fzf
-sudo apt install -y python3-pip
+sudo apt install -y \
+	ripgrep \
+	neovim \ 
+	fd-find \
+	fzf \
+	python3-pip \
+	pre-commit \
+	bat
+
+echo "Go to https://github.com/dandavison/delta/releases and get delta"
--- fish/config.fish
+++ fish/config.fish
@@ -5,10 +5,14 @@ alias vim "nvim"
 alias gpg "gpg2"
 alias xstart "xdg-open"
 alias fd "fdfind"
+alias bat "batcat"
 
-set FZF_DEFAULT_COMMAND 'fd --type f'
+set FZF_DEFAULT_COMMAND 'fd --type=f'
+set FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set FZF_ALT_C_COMMAND 'fd --type=d'
+
 
 set -x PATH "$HOME/.local/bin" $PATH
-# fish_vi_key_bindings
+fish_vi_key_bindings
 # bind -M insert \cf accept-autosuggestion
 
