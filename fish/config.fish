cb3d564e7c650fa4ad1abc6a0fa9497153412028
Bor Gonzalez-Usach <bor.gonzalez.usach@contact-software.com>
2023-07-26 11:40:08 +0200

    Minor improvement

--- fish/config.fish
+++ fish/config.fish
@@ -12,15 +12,10 @@ set FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
 set FZF_ALT_C_COMMAND 'fd --type=d'
 
 
-set -x PATH "$HOME/.local/bin" $PATH
-
-# The standard fish functions dir is git-tracked. To be able to define functions
-# without tracking them, add another dir.
-set -x fish_function_path $fish_function_path "$HOME/.config/fish/additional-functions"
+fish_add_path "$HOME/.local/bin"
 
 fish_vi_key_bindings
 # bind -M insert \cf accept-autosuggestion
 
 # Bind reverse search to <ctrl-h> (history), so that <ctrl-r> can be used within neovim terminal mode
-bind -M insert \ch fzf-history-widget  
-
+bind -M insert \ch fzf-history-widget
